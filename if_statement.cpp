5c5e9aaf887c2a36beae3f8da64fbc1ca804a7e6
mittulofficial <111638490+mittulofficial@users.noreply.github.com>
2023-06-30 18:32:12 +0530

    Create if_statement.cpp
    this code is about if statement only

---
+++ if_statement.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  Lecture4 I: Conditional statements
+//
+//  Created by MITTUL KUMAR on 26/06/23.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+   
+    float a,b;
+    cout<<"give 2 floating point numbers as input"<<endl;
+    cin>>a>>b;
+    if(a>0.01 && b>a)
+    {
+        cout<<"the condition in if statement is correct";
+    }
+    return 0;
+}
