71b48ffe92b8ec84d49292df159e62fd34c13f29
mittulofficial <111638490+mittulofficial@users.noreply.github.com>
2023-07-08 20:12:15 +0530

    Create elseifmoreexample.cpp
    more exmple in if else if

---
+++ elseifmoreexample.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  Lecture4 I: Conditional statements
+//
+//  Created by MITTUL KUMAR on 26/06/23.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+   
+    int  a,b,c,d;
+    cout<<"enter value";
+    cin>>a>>b>>c;
+    
+    if(a>b && c>d)
+    {
+        cout<<"haha";
+        
+    }
+    else if(a>b && c<d)
+    {
+        cout<<"hehe";
+        
+    }
+    else
+    {
+        cout<<"huhu";
+    }
+    return 0;
+    
+}
